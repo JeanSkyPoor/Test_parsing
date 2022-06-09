6a094cfdaa8b8865e5ef3d8e454741dd0e1f7432
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-09 18:43:21 +0300

    drop second link

--- First_link/main.py
+++ First_link/main.py
@@ -3,7 +3,6 @@ from secondary_defs import *
 
 
 link_1 = 'https://api.kfc.com/api/store/v2/store.get_restaurants?showClosed=true'
-link_2 = 'https://www.ziko.pl/lokalizator/'
 
 #Solve first link
 data = create_request_and_return_json_first_link(link_1)
