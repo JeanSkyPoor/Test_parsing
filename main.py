a440ffd588b0d6a32cabe58984c3a070a33a8f60
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-09 15:40:39 +0300

    delete imports

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
 from secondary_defs import *
-import json
+
 
 
 link_1 = 'https://api.kfc.com/api/store/v2/store.get_restaurants?showClosed=true'
+link_2 = 'https://www.ziko.pl/lokalizator/'
 
 #Solve first link
 data = create_request_and_return_json_first_link(link_1)
